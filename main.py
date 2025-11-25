191b91b7eacf07165f5002717d22b83aa49450c3
Jacob Vidal <firstcommand@gmail.com>
2025-11-25 11:15:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from app.payments import PagoTarjeta  # o PagoPayPal, PagoCrypto, etc.
+# from app.payments import PagoCrypto
 from app.services import PedidoService
 from app.cli import PedidoCLI
 
 
 def main() -> None:
     metodo_pago = PagoTarjeta()
+    # metodo_pago = PagoCrypto()
     service = PedidoService(metodo_pago)
     cli = PedidoCLI(service)
     cli.ejecutar_demo()
